f461498705a79996c1d98308ce566cdd1f55272e
Ru521 <yuru880521@gmail.com>
2021-07-22 16:11:15 +0800

    page add v-carousel

--- src/views/VBInstall/VBInstall.vue
+++ src/views/VBInstall/VBInstall.vue
@@ -3,26 +3,40 @@
     <div class="py-4">
       <div class="d-flex justify-content-between w-100 flex-wrap">
         <div class="mb-3 mb-lg-0">
-          <h1 class="h4">VB 安裝</h1>
+          <h1 class="h1">VB 安裝</h1>
           <p class="mb-0">
-            Dozens of reusable components built to provide buttons, alerts,
-            popovers, and more.
+            Visual Studio Community 2015 安裝步驟說明
           </p>
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-12 mb-4">
-        <div class="card border-0 shadow">
-          <div class="card-body">
-            <div class="row d-block mt-4">
-              <div class="col-lg-4">
-                <!-- Button Modal -->
-                123
-              </div>
-            </div>
-          </div>
-        </div>
+
+    <div class="col-12 mb-4">
+      <div class="mb-3 mb-lg-0">
+        <h1 class="h4">創建帳號</h1>
+        <p class="mb-0">
+          創建微軟帳號，或是使用GitHub帳號也可以
+        </p>
+      </div>
+    </div>
+    <div class="col-12 mb-4">
+      <div class="mb-3 mb-lg-0">
+        <v-carousel height="400" hide-delimiter-background show-arrows>
+          <v-carousel-item>
+            <v-sheet color="indigo" height="100%">
+              <v-row class="fill-height" align="center" justify="center">
+                <div class="white--text">Slide</div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+          <v-carousel-item>
+            <v-sheet color="indigo" height="100%">
+              <v-row class="fill-height" align="center" justify="center">
+                <h2>Slide</h2>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
       </div>
     </div>
   </v-row>
