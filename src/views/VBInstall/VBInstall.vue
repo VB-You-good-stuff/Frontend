256a6c74d29d16cccb8ae58b50039498db1ed5ce
Ru521 <yuru880521@gmail.com>
2021-07-26 12:44:23 +0800

    title add bold

--- src/views/VBInstall/VBInstall.vue
+++ src/views/VBInstall/VBInstall.vue
@@ -3,7 +3,7 @@
     <div class="py-4">
       <div class="d-flex justify-content-between w-100 flex-wrap">
         <div class="mb-3 mb-lg-0">
-          <h1 class="display-1">VB 安裝</h1>
+          <h1 class="display-1 bold">VB 安裝</h1>
           <p class="mb-0 h4">
             Visual Studio Community 2015 安裝步驟說明
           </p>
